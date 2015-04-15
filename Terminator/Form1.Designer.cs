94f84a3c82901f8b97e75b27b37b8cbf38104068
Devin <devin@interviewmaster.in>
2015-04-15 04:30:47 -0400

    first commit

---
+++ Terminator/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Terminator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(69, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hibernate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(383, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Shutdown";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(595, 462);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Terminator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+
+
+
+    }
+}
+
---
+++ Terminator/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Terminator.Properties;
+
+namespace Terminator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            GenerateTimerButtons();
+        }
+
+        private void GenerateTimerButtons()
+        {
+            //intervals in minutes
+            List<double> intervals = new List<double>();
+            
+            //interval opetions (in minutes)
+            intervals.Add(30);
+            intervals.Add(60);
+            intervals.Add(90);
+            intervals.Add(120);
+            intervals.Add(180);
+            intervals.Add(240);
+
+            //initial offset for the buttons (in relativ pixels)
+            int BtnYaxis = 1;
+
+            //generate the desired buttons
+            foreach (double Ctr in intervals)
+            {
+                Button HibernateBtn = new Button();
+
+                HibernateBtn.Location = new Point(45, 60 * BtnYaxis + 30);
+                HibernateBtn.Size = new System.Drawing.Size(150, 45);
+                HibernateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                HibernateBtn.UseVisualStyleBackColor = true;
+                HibernateBtn.Click += new EventHandler(HibernateHandler);
+                HibernateBtn.Tag = (Ctr * 60 * 1000).ToString(); //convert minutes to milliseconds.
+                HibernateBtn.Text = ConvertMinToHourText(Ctr);
+                Controls.Add(HibernateBtn);
+
+                Button ShutdownBtn = new Button();
+
+                ShutdownBtn.Location = new Point(350, 60 * BtnYaxis + 30);
+                ShutdownBtn.Size = new System.Drawing.Size(150, 45);
+                ShutdownBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                ShutdownBtn.UseVisualStyleBackColor = true;
+                ShutdownBtn.Click += new EventHandler(ShutdownHandler);
+                ShutdownBtn.Tag = (Ctr * 60 * 1000).ToString(); //convert minutes to milliseconds.
+                ShutdownBtn.Text = ConvertMinToHourText(Ctr);
+                Controls.Add(ShutdownBtn);
+
+                BtnYaxis++;
+            }
+
+        }
+
+        private string ConvertMinToHourText(double Minutes)
+        {
+            if (Minutes < 60)
+                return Minutes + " mins";
+            else
+            {
+                return (Minutes / 60).ToString("0.##") + " hrs";
+            }
+        }
+
+        private async void HibernateHandler(object sender, EventArgs e)
+        {
+            DisableControls();
+
+            Button button = sender as Button;
+            button.BackColor = Color.YellowGreen;
+            
+            int Delay = int.Parse(button.Tag.ToString());
+            await Task.Delay(Delay);
+            Process.Start("shutdown", "/h");
+        }
+
+        private async void ShutdownHandler(object sender, EventArgs e)
+        {
+            DisableControls();
+
+            Button button = sender as Button;            
+            button.BackColor = System.Drawing.Color.Crimson;
+            button.ForeColor = System.Drawing.SystemColors.ButtonFace;
+                       
+
+            int Delay = int.Parse(button.Tag.ToString());
+            await Task.Delay(Delay);
+            Process.Start("shutdown", "/s");
+        }
+
+
+        private void DisableControls()
+        {
+            foreach (Control c in Controls)
+            {
+                Button b = c as Button;
+                if (b != null)
+                {
+                    b.BackColor = System.Drawing.SystemColors.ButtonShadow;
+                    b.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                    b.Enabled = false;
+                }
+            }
+
+        }
+
+        private void EnableControls(Control con)
+        {
+            if (con != null)
+            {
+                con.Enabled = true;
+                EnableControls(con.Parent);
+            }
+        }
+          
+     
+    }
+}
